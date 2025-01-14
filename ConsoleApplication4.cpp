11afce29eaf6af3bc15424b9dd5183fc6ecc2260
prostokovich <160667444+prostokovich@users.noreply.github.com>
2025-01-14 16:40:39 +0300

    lab4

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath> 
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << std::fixed << std::setprecision(3);
+
+    for (double t = 0; t <= 3; t += 0.5) {
+        double y = std::cos(t);
+
+        std::cout << "t = " << std::setw(10) << t << "  y = " << std::setw(10) << y << std::endl;
+    }
+
+    return 0;
+}
